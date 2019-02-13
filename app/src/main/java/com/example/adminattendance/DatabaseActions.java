cdcd8aa4138cded2ecfc85221701027d781d059f
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-02-13 12:55:19 +0530

    finalizing

--- app/src/main/java/com/example/adminattendance/DatabaseActions.java
+++ app/src/main/java/com/example/adminattendance/DatabaseActions.java
@@ -484,6 +484,55 @@ public class DatabaseActions extends AsyncTask<String,Void,String>
                 e.printStackTrace();
             }
         }
+        else if (type.equals("get_admin_details"))
+        {
+            String login_url = base_url + "get_admin_details.php";
+            try
+            {
+                String user_id = params[1];
+
+                //connecting with server
+                url = new URL(login_url);
+                HttpURLConnection httpURLConnection = null;
+                httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+
+                //sending data to the server
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+
+                String post_data = URLEncoder.encode("user_id","UTF-8")+"="+URLEncoder.encode(user_id,"UTF-8");
+
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                //getting the data coming from server
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+
+                result="";
+                String line;
+
+                while((line = bufferedReader.readLine())!= null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         return result;
     }
--- app/src/main/java/com/example/adminattendance/SettingsFragment.java
+++ app/src/main/java/com/example/adminattendance/SettingsFragment.java
@@ -1,6 +1,138 @@
 package com.example.adminattendance;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 public class SettingsFragment extends Fragment {
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    ListView settingsLV;
+    TextView text;
+    TextView studentDetails;
+
+    String data[] = {"Add Course", "Delete Course", "Log Out"};
+    ArrayAdapter<String> adapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_settings, null);
+
+        settingsLV = view.findViewById(R.id.settingsLV);
+        text = view.findViewById(R.id.text);
+        studentDetails = view.findViewById(R.id.studentDetails);
+
+        text.setText("Settings");
+
+        //getting the info of the logged user
+        sharedPreferences = this.getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        String user_id_cookie = sharedPreferences.getString("user_id", "DNE");
+        editor = sharedPreferences.edit();
+
+        String user_id = new Encryption().decrypt(user_id_cookie);
+
+        //checking if phone if connected to net or not
+        ConnectivityManager connMgr = (ConnectivityManager) getActivity()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected()) //phone is connected
+        {
+            //showing prof details
+            String studentDetailsResults = null;
+            try {
+                String type = "get_admin_details";
+                studentDetailsResults = new DatabaseActions().execute(type, user_id).get();
+
+                //parse JSON and getting data
+                JSONArray ja = new JSONArray(studentDetailsResults);
+                JSONObject jo = ja.getJSONObject(0);
+
+                String name = jo.getString("name");
+                String username = jo.getString("username");
+                String email_id = jo.getString("email_id");
+                String phone = jo.getString("phone");
+
+                studentDetails.setText("Name: " + name + "\nUsername: " + username + "\nEmail ID: " + email_id + "\nPhone No: " + phone);
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            studentDetails.setText("");
+        }
+
+        //listing setting options in listview
+        adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, data);
+        settingsLV.setAdapter(adapter);
+
+        //on clicking on any list item
+        settingsLV.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+            {
+                //on clicking on setting options
+                String listViewText = ((TextView)view).getText().toString();
+
+                if(listViewText.equals(data[0])) //add course
+                {
+                    //redirecting to the manage courses page
+                    Intent manageCoursePage = new Intent(getActivity().getApplicationContext(), AddCourse.class);
+                    startActivity(manageCoursePage);
+                }
+                if(listViewText.equals(data[1])) //add course
+                {
+                    //redirecting to the manage courses page
+                    Intent manageCoursePage = new Intent(getActivity().getApplicationContext(), DeleteCourse.class);
+                    startActivity(manageCoursePage);
+                }
+                else if(listViewText.equals(data[2])) //logout
+                {
+                    //removing all cookies
+                    editor.remove("user_id");
+                    editor.remove("username");
+                    editor.remove("course_id");
+                    editor.remove("studentCourses");
+                    editor.commit();
+
+                    //redirecting to the login page
+                    Intent loginPage = new Intent(getActivity().getApplicationContext(), MainActivity.class);
+                    startActivity(loginPage);
+                    getActivity().finish();
+                }
+            }
+        });
+
+        return view;
+    }
 }
